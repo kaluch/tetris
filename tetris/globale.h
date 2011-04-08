59e5c3869defac8bc7a669c05cc726c875093720
Lucas SOUCHET <kalu.souch@gmail.com>
2011-04-08 10:02:14 +0200

    ajout globale.h

---
+++ tetris/globale.h
@@ -0,0 +1,14 @@
+/*
+ * globale.h
+ *
+ *  Created on: 8 avr. 2011
+ *      Author: souchet
+ */
+
+#ifndef GLOBALE_H_
+#define GLOBALE_H_
+
+#define HAUTEUR_ECRAN 600
+#define LARGEUR_ECRAN 800
+
+#endif /* GLOBALE_H_ */
